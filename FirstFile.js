ae832ad92aee3e3adc4f914486af4486c0028400
AshisMishra <ash.ishu025@gmail.com>
2022-10-20 21:21:03 +0530

    First Commit For Git Setup

---
+++ FirstFile.js
@@ -0,0 +1,10 @@
+console.log("This is the first code like - Hello World");
+
+
+function PrintName(name) {
+    console.log(name);
+}
+
+PrintName("MERNStack - is the session name!!")
+
+PrintName("MeheJabeen")
