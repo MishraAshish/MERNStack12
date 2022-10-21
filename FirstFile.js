b0e952fbd632b645fb52969bd25d918e6bbdfbdc
AshisMishra <ash.ishu025@gmail.com>
2022-10-21 21:24:34 +0530

    Javascript Basics

--- FirstFile.js
+++ FirstFile.js
@@ -1,10 +1,20 @@
 console.log("This is the first code like - Hello World");
 
+PrintName("MERNStack - is the session name!!") //due to hoisting i it will execute before definition
+PrintName("MeheJabeen")
+PrintName("Ang Li")
 
+//Function - are first class member citizens of Javascript
 function PrintName(name) {
-    console.log(name);
+    console.log(name);//context or execution context (this - keyword)
 }
 
-PrintName("MERNStack - is the session name!!")
+var name = "A"
+var name = "B"
 
-PrintName("MeheJabeen")
+
+//Class - Some Purpose
+// class Human { //Employee, Student, Scientist ---
+//     //Method - Behaviour
+//     GetName()// Should Be Common
+// }
