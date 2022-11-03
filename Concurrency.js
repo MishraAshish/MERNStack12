1c493c3ff9dd8fdaed7fe66d3b9f1a1ac156fa45
AshisMishra <ash.ishu025@gmail.com>
2022-11-03 21:27:17 +0530

    event loop

---
+++ Concurrency.js
@@ -0,0 +1,20 @@
+//Jason, Paribesh, Ang Li
+
+//Order Prep Time
+//Jason - 5 min
+//Ang Li - 10 min
+//Paribesh - 15 min
+
+//Restuarant - 1
+//Waiter -
+//Kitchen -
+
+//Total Time - 5+10+15 => 30 Min //Java, .net by default work in sync (waiting)
+
+
+//Restuarant - 2
+//Waiter -
+//Kitchen -
+//5, 10, 15 => all the orders would taken together and prepared
+
+//Total Time -> 15 => 15 Min //Taking the maximum time order js/nodejs , callbacks, event loop, async
