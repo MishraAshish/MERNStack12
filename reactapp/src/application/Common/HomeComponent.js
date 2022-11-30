42dee724d7f63e00a36bc4cab91e556b08f4db6a
AshisMishra <ash.ishu025@gmail.com>
2022-11-30 21:55:47 +0530

    setState forceUpdate componentLifeCycleMethods event bibinding

---
+++ reactapp/src/application/Common/HomeComponent.js
@@ -0,0 +1,84 @@
+import React, { Component, PureComponent } from "react";
+
+//export default class HomeComponent extends Component {
+export default class HomeComponent extends PureComponent { //Pure Component has implementation of shouldComponentUpdate
+    constructor(props){
+        super();
+        this.state = {
+            age : 20,
+            name : "Synergistic IT"
+        }
+        //getAllDistricts() - server call not allowed here
+        //we can't access the html
+    }
+
+    //creation life cycle method
+    componentDidMount(){
+        console.log("Component Did Mount - Rendered On Browser");
+        //we can access the html and make call to server to fetch more data
+        //getAllDistricts()
+    }
+
+    //fetch the list of districts - ajax call
+    // getAllDistricts(countryidFromIPAdress){
+    // }    
+
+    //update life cycle method
+    // shouldComponentUpdate(nextProps, nextState){
+    //     console.log(nextState)
+    //     console.log(nextProps)
+
+    //     if (nextState.age == this.state.age) {//if the updated value of age is same then don't call render method
+            
+    //         return false;//it will not call the render method so the new v-dom will not be created
+    //     } 
+    //     return true; //it will invoke the render method
+    // }
+
+    getSnapshotBeforeUpdate(prevState, prevProps){
+        console.log("getSnapshotBeforeUpdate");
+        console.log("prevState", prevState);
+        console.log("prevProps", prevProps);
+        return {
+            prevState,
+            prevProps
+        }
+    }
+
+    componentDidUpdate(prevState, prevProps){
+        console.log("componentDidUpdate");
+        console.log("prevState",prevState);
+        console.log("prevProps", prevProps);
+    }
+
+    clickEventHandler = (evt)=>{
+        //alert("Button is Clicked!!")
+        this.setState({
+            age : this.state.age //+ 1
+        })
+
+        // this.state.age++;
+        // this.forceUpdate();
+    }
+
+
+    //destruction life cycle method
+    componentWillUnmount(){
+        //we should use this LC method to remove all the subscriptions and any calls like setinterval 
+        console.log("Component Will Unmount")
+    }
+
+    //creation and updation life cycle method
+    render(){
+        console.log("Home Render!!");
+
+        return(
+            <>
+                <h2>{this.props.title}</h2>
+                 <label> {this.state.age} </label>
+                <button onClick={this.clickEventHandler}>Increment Age</button>
+            </>
+        )
+    }
+
+}
--- reactapp/src/application/app.js
+++ reactapp/src/application/app.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
 import Header from "./Common/HeaderComponent";
 import Footer from "./Common/FooterComponent";
+import Home from "./Common/HomeComponent";
 
 export default class ApplicationComponent extends Component{
-    
+    constructor(props, context){
+        super();
+
+        this.state = {
+            title : "Home Page Of Application",
+            counter : 1
+        }
+
+        //this.increment();
+    }
+
+    increment = ()=>{
+        setInterval(() => {
+            this.state.counter++;
+            this.setState({ // the reseved API to pass the message to react library to invoke render method so that new v-dom can be created
+                counter : this.state.counter + 1
+            })
+
+            //we should avoid to use unless necessary
+            //this.forceUpdate();//force update also calls render method but skips other life cycle methods
+            console.log(this.state.counter);
+        }, 1000);
+    }
+
     render(){
+        console.log("Application Render!!");
         let myName = "What's in the Name";
         let address = "Somewhere on earth";
         let val1 = 2, val2 =5;//state and props
@@ -12,8 +37,11 @@ export default class ApplicationComponent extends Component{
         return(
             <>
                 <Header/>
-
-                <div>
+                
+                <h4>{this.state.counter}</h4>
+                
+                <Home title={this.state.title}/>
+                {/* <div>
                     <h1>The React Page</h1>
 
                     <h4>{myName}</h4>
@@ -25,7 +53,7 @@ export default class ApplicationComponent extends Component{
 
                     <h4>{val1 + val2}</h4>
                     <h6>{val1 * val2}</h6>
-                </div>
+                </div> */}
 
                 <Footer address1={address}/>
             </>
