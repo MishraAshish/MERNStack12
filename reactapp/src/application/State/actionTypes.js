6b2dfb475b368996fde2b240affdce2ab6a10899
AshisMishra <ash.ishu025@gmail.com>
2022-12-14 20:09:16 +0530

    project day 1

--- reactapp/src/application/State/actionTypes.js
+++ reactapp/src/application/State/actionTypes.js
@@ -1,3 +1,5 @@
 //collection of action types to be used in actions and reducers
 
 export const USER_ADDUSER = "USER_ADDUSER";//adduser should be the action
+
+export const STUDENT_ADDSTUDENT = "STUDENT_ADDSTUDENT";
