6d5e661b13be51760013aba8b60467238f9450c3
Ashish Mishra <ash.ishu025@gmail.com>
2023-01-09 21:34:29 +0530

    mernstack wrap

--- reactapp/src/application/app.js
+++ reactapp/src/application/app.js
@@ -1,23 +1,23 @@
-import React, { Component,Suspense } from "react";
+import React, { Component, lazy, Suspense } from "react";
 import {BrowserRouter as Router, Routes, Redirect, Route} from "react-router-dom";// browser router from react
 
 import "./app.css";//css and style loaders are present in webpack config so we can use it here
 
-import Header from "./Common/HeaderComponent";
-import Footer from "./Common/FooterComponent";
-import Home from "./Common/HomeComponent";
-import About from "./Common/AboutComponent";
-import NotFound from "./Common/NotFoundComponent";
+let Header = lazy(()=> import("./Common/HeaderComponent"));
+let Footer = lazy(()=> import("./Common/FooterComponent"));
+let Home = lazy(()=> import("./Common/HomeComponent"));
+let About = lazy(()=> import("./Common/AboutComponent"));
+let NotFound = lazy(()=> import("./Common/NotFoundComponent"));
 //import User from "./AppComponents/UserComponent/UserComponent";
-import User from "./AppComponents/UserContainer/UserContainer";
-import UserHook from "./AppComponents/UserComponent/UserHooks";
-import UsingHooks from "./Hooks/UnderstandingHooks";
-import ProductComponent from "./AppComponents/Product/ProductComponent";
-import DisplayProducts from "./AppComponents/Product/DisplayProduct";
-import CartComponent from "./AppComponents/Cart/CartComponent";
-import Checkout from "./AppComponents/Checkout/CheckoutComponent"
-import Coupon from "./AppComponents/Coupon/CouponComponent";
-import Hooks from "./Hooks/UsingHooksMemo";
+//import User from "./AppComponents/UserContainer/UserContainer";
+let UserHook = lazy(()=> import("./AppComponents/UserComponent/UserHooks"));
+//import UsingHooks from "./Hooks/UnderstandingHooks";
+let ProductComponent = lazy(()=> import("./AppComponents/Product/ProductComponent"));
+let DisplayProducts = lazy(()=> import("./AppComponents/Product/DisplayProduct"));
+let CartComponent = lazy(()=> import("./AppComponents/Cart/CartComponent"));
+let Checkout = lazy(()=> import("./AppComponents/Checkout/CheckoutComponent"));
+let Coupon = lazy(()=> import("./AppComponents/Coupon/CouponComponent"));
+//import Hooks from "./Hooks/UsingHooksMemo";
 
 export default class ApplicationComponent extends Component{
     constructor(props, context){
@@ -38,26 +38,27 @@ export default class ApplicationComponent extends Component{
         //JSX - Javascript Like XML 
         return(
             <Router>
-                <Suspense fallback={<div>Loading...</div>}></Suspense>
-                <Header headerTitle={this.state.headerTitle}/>                
-                {/* Here we'll use switch based conditions to render one component at a time */}
-                <Routes>
-                        <Route path="/" element={<Home  title={this.state.title} />} />
-                        <Route path="/home" element={<Home  title="Home Component" />} />
-                        <Route path="/user" element={<UserHook />} />
-                        <Route path="/product" element={<ProductComponent />} />
-                        <Route path="/cart" element={<CartComponent />} />
-                        <Route path="/display" element={<DisplayProducts />} />
-                        <Route path="/checkout" element={<Checkout />} />
-                        <Route path="/coupon" element={<Coupon />} />
-                        {/* <Route path="/hooks" element={<UsingHooks  name={"Mehejabeen"}/>} /> */}
-                        <Route path="/about" element={<About />} />
-                        <Route path="/hooks" element={<Hooks />} />
-                        <Route path="/about/:id" element={<About />} />
-                        <Route path="*" element={<NotFound />} />
-                </Routes>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Header headerTitle={this.state.headerTitle}/>                
+                    {/* Here we'll use switch based conditions to render one component at a time */}
+                    <Routes>
+                            <Route path="/" element={<Home  title={this.state.title} />} />
+                            <Route path="/home" element={<Home  title="Home Component" />} />
+                            <Route path="/user" element={<UserHook />} />
+                            <Route path="/product" element={<ProductComponent />} />
+                            <Route path="/cart" element={<CartComponent />} />
+                            <Route path="/display" element={<DisplayProducts />} />
+                            <Route path="/checkout" element={<Checkout />} />
+                            <Route path="/coupon" element={<Coupon />} />
+                            {/* <Route path="/hooks" element={<UsingHooks  name={"Mehejabeen"}/>} /> */}
+                            <Route path="/about" element={<About />} />
+                            {/* <Route path="/hooks" element={<Hooks />} /> */}
+                            <Route path="/about/:id" element={<About />} />
+                            <Route path="*" element={<NotFound />} />
+                    </Routes>
 
-                <Footer address1={address}/>
+                    <Footer address1={address}/>
+                </Suspense>
             </Router>
         )
     }
